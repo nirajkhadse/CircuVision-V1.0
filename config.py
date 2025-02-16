92e981a085e08cefbcc4c79281ddc2c77cb93a74
Niraj <65868711+nirajkhadse@users.noreply.github.com>
2025-02-16 16:25:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    "BRAND_NAME": "CircuVision.AI",
+    "YOLO_PATH": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\AI_Models\\YOLOv5",
+    "MODEL_WEIGHTS": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\AI_Models\\YOLOv5\\runs\\train\\exp4\\weights\\best.pt",
+    "UPLOADS_PATH": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\User_Interface\\uploads",
+    "RESULTS_PATH": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\User_Interface\\results",
+    "CROPPED_PATH": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\User_Interface\\cropped",
+    "REPORTS_PATH": "C:\\Users\\niraj\\Documents\\CircuVision.AI\\User_Interface\\reports"
+}
